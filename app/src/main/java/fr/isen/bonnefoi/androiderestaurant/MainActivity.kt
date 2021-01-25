4c13d74999384f92b046ff933685cc15cfc0c58b
JejeB <jeremy.bonnefoi@isen.yncrea.fr>
2021-01-25 13:30:12 +0100

    Add buttons

--- app/src/main/java/fr/isen/bonnefoi/androiderestaurant/MainActivity.kt
+++ app/src/main/java/fr/isen/bonnefoi/androiderestaurant/MainActivity.kt
@@ -2,10 +2,26 @@ package fr.isen.bonnefoi.androiderestaurant
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    fun EntreCallBack(view: View?){
+        val mytoast = Toast.makeText(this.applicationContext,"Entrées",Toast.LENGTH_SHORT)
+        mytoast.show()
+    }
+    fun PlatCallBack(view: View?){
+        val mytoast = Toast.makeText(this.applicationContext,"PlatCallBack",Toast.LENGTH_SHORT)
+        mytoast.show()
+    }
+    fun DessertCallBack(view: View?){
+        val mytoast = Toast.makeText(this.applicationContext,"Dessert",Toast.LENGTH_SHORT)
+        mytoast.show()
+    }
+
 }
