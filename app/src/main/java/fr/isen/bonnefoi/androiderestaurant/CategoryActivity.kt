3ab5ad4cdb0b5b9d682cacc32f678f90764cdbb7
JejeB <jeremy.bonnefoi@isen.yncrea.fr>
2021-01-26 17:33:56 +0100

    J'ai eu un probleme sur mon emulateur j'ai du faire un gros rollback à 12h -_-

--- app/src/main/java/fr/isen/bonnefoi/androiderestaurant/CategoryActivity.kt
+++ app/src/main/java/fr/isen/bonnefoi/androiderestaurant/CategoryActivity.kt
@@ -20,39 +20,8 @@ class CategoryActivity: AppCompatActivity() {
         var helloTextView = findViewById<TextView>(R.id.titleEntre);
         helloTextView.text = this.category;
     }
-    private fun getFoodList() : List<Item>{
-        var ret : List<Item> = emptyList()
-        val queue = Volley.newRequestQueue(this)
-        val url = "http://test.api.catering.bluecodegames.com/menu"
-        val params = JSONObject()
-        params.put("id_shop", "1")
 
-        // Request a string response from the provided URL.
-        val stringRequest = JsonObjectRequest(Request.Method.POST, url,params,
-                Response.Listener<JSONObject> { response ->
-                    Log.i("json","Response is: $response")
-                    val gson = Gson()
-                    val json = gson.fromJson(response.toString(),FoodDataJSON::class.java)
-                    Log.d("test",""+json)
 
-                    when(category){
-                        "Entrées" -> ret = getAllFoodItems(json.data[0])
-                        "Plats" -> ret = getAllFoodItems(json.data[1])
-                        "Desserts" -> ret = getAllFoodItems(json.data[2])
-                    }
-                    binding.listCategory.adapter = CategoryListAdapter(ret)
-                    setContentView(binding.root)
-                },
-                Response.ErrorListener {
-                    Log.i("test","request failed")
-                }
-        )
-
-        // Add the request to the RequestQueue.
-        queue.add(stringRequest)
-
-        Log.d("ty",""+ret)
-        re
-
+      
 
     }
